2679c7b08fce419defcdfb1b1beff4bdc7bf1ec8
Ameer Hamza <ameerhamzag75@gmail.com>
2026-03-30 15:24:56 +0500

    data types in js

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,12 +1,46 @@
 console.log('Hello I\m here')
 
 // Variables
-
+var name = "Ameer Hamza"
 
 // scope variablename = value/data
-
 const myName = "Ameer Hamza"
+const myAge = 'twenty two'
+const myAge1 = `twenty two`
 
+const myAge2 = 1234
+
+
+const isLogin = false;
+
+let myName1= undefined;
+
+myName1 = "Ameer Hamza"
+
+const xyz = Symbol('xyz')
+
+const value1 = null
 console.log(myName)
 
 
+
+const employe ={
+    name: "Ameer Hamza",
+    age: 22,
+    isLogin: false,
+    myName1: undefined,
+    xyz: Symbol('xyz'),
+    value1: null
+}
+
+const nums = [1,2,3,4,5, "ameer", true, undefined, null, Symbol('xyz')]
+
+
+
+{
+    const name1 = "Ameer Hamza"
+    let name2 = "Ameer Hamza"
+    name2 = "Hamza"
+
+    console.log(name1)
+}
