88aff5dff212058a1079c08fe063beab789db8c9
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2026-04-23 18:31:04 +0500

    implement static image

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Navbar.css'
+import NavLogo from '../../../public/download.jpeg'
 function Navbar(props) {
   const {name, phone}=props
+
   return (
     <div style={{ backgroundColor: "blue", height: "100px", width: "100%", color: 'white' }}>
+      {/* <img src="/download.jpeg" alt="" /> */}
+      <img src={NavLogo} alt="" width="100" height="50"/>
       <h1 className=''>{name}</h1>
       <strong>{phone}</strong>
     </div>
