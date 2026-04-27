8343ee8e3144e0dce910a037ecf3e98cedb2f4c2
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2026-04-27 18:52:24 +0500

    state hook

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Sales from './pages/Sales'
 
 function App() {
+  // Define Hooks
+  const [teacherName, setTeacherName] = useState("MEER HAMZA")
+  const [count, setCount] = useState(0)
+  // let teacherName = "Ameer Hamza"
+
+  function tNameChange() {
+    console.log('first')
+    // teacherName = "Ali Hamza"
+
+    if (teacherName == "MEER HAMZA") {
+      setTeacherName("Ali Hamza")
+    }
+    if (teacherName == "Ali Hamza") {
+      setTeacherName("MEER HAMZA")
+    }
+    console.log(teacherName)
+  }
+
+
+  // Count
+
+  function counting() {
+    setCount(count + 1)
+  }
+  // Baki kaam
+
   const myName = "Ameer"
   console.log('Hello', myName)
 
@@ -12,11 +39,18 @@ function App() {
 
   return (
     <div>
-      <Navbar name={name} phone={contact}/>
+      <Navbar name={name} phone={contact} />
       {/* <h1 className='xyz'>Hello {myName}</h1>
       <Navbar name="Arslan" phone="0909999999"/> */}
-      <Sales/>  
-      <Footer/>
+      {/* <Sales/>   */}
+
+      <h1>{teacherName}</h1>
+
+      <button onClick={tNameChange}>Click me</button>
+
+      <h2>Count: {count}</h2>
+      <button onClick={counting}>Count++</button>
+      <Footer />
     </div>
   )
 }
