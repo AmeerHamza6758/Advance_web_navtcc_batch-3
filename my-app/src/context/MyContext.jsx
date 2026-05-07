a71f16bb13b6877a8aaa95e0faa35275bb970fc9
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2026-05-07 18:03:42 +0500

    api integration

--- my-app/src/context/MyContext.jsx
+++ my-app/src/context/MyContext.jsx
@@ -1,6 +1,4 @@
 import React, { createContext, useState } from "react";
-
-
 // Create context
 export const MyContext = createContext("")
 
--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,12 +1,52 @@
-import React, { use, useContext } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import { MyContext } from '../context/MyContext'
 
 function Home() {
-  const {name}=use(MyContext)
+  const [data, setData] = useState()
+
+  // Get Data
+  function fetchData() {
+    const response = fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((data) => data.json())
+      .then((res) => console.log('API KA RESPONSE', setData(res)))
+      .catch((err) => console.log(err))
+  }
+
+  // useEffect
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const { name } = use(MyContext)
   return (
-    <div style={{height:"400px"}}>
+    <div style={{ height: "400px" }}>
 
       <h1>Hello my name is: {name}</h1>
+
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: "200px 200px 200px",
+        backgroundColor: 'lightblue'
+      }}>
+        <strong>ID</strong>
+        <strong>USERID</strong>
+        <p>Title</p>
+      </div>
+
+      {data.map((item, index) => (
+        <div key={index || item.id} style={{
+          display: 'grid',
+          gridTemplateColumns: "200px 200px 200px",
+          backgroundColor: 'lightcyan'
+        }}>
+          <strong>{item.id}</strong>
+          <strong>{item.userId}</strong>
+          <p>{item.title}</p>
+        </div>
+      ))
+      }
+
+
     </div>
   )
 }
